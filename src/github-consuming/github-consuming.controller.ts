879a6e85cf6961d16efdb027b1046a39a82b8b1d
cybenuki <danfeedev@gmail.com>
2024-05-30 18:43:23 -0500

    refactor: change github-consuming.controller tag to @ApiTags('first exercise')

--- src/github-consuming/github-consuming.controller.ts
+++ src/github-consuming/github-consuming.controller.ts
@@ -4,7 +4,7 @@ import { AllRepositoryParams } from './params/all-repositories.params';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AllRepositoriesResponseDTO } from './dto/all-repositories-response.dto';
 
-@ApiTags('first Question')
+@ApiTags('first exercise')
 @Controller('github-consuming')
 export class GithubConsumingController {
   constructor(private readonly githubConsumingService: GithubConsumingService) { }
